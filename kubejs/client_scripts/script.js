fe6d57750c573f7209b768e5875d827d7b8ffee4
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2021-11-08 15:23:56 +0100

    Create script.js

---
+++ kubejs/client_scripts/script.js
@@ -0,0 +1,96 @@
+// priority: 0
+
+console.info('Hello, World! (You will see this line every time client resources reload)')
+
+onEvent('jei.hide.items', event => {
+	// Hide items in JEI here
+	event.hide('indrev:tin_sword')
+	event.hide('indrev:steel_sword')
+	event.hide('indrev:silver_sword')
+	event.hide('indrev:lead_sword')
+	event.hide('indrev:copper_sword')
+	event.hide('indrev:bronze_sword')
+
+	event.hide('indrev:tin_shovel')
+	event.hide('indrev:steel_shovel')
+	event.hide('indrev:silver_shovel')
+	event.hide('indrev:lead_shovel')
+	event.hide('indrev:copper_shovel')
+	event.hide('indrev:bronze_shovel')
+
+	event.hide('indrev:tin_pickaxe')
+	event.hide('indrev:steel_pickaxe')
+	event.hide('indrev:silver_pickaxe')
+	event.hide('indrev:lead_pickaxe')
+	event.hide('indrev:copper_pickaxe')
+	event.hide('indrev:bronze_pickaxe')
+
+	event.hide('indrev:tin_axe')
+	event.hide('indrev:steel_axe')
+	event.hide('indrev:silver_axe')
+	event.hide('indrev:lead_axe')
+	event.hide('indrev:copper_axe')
+	event.hide('indrev:bronze_axe')
+
+	event.hide('indrev:tin_hoe')
+	event.hide('indrev:steel_hoe')
+	event.hide('indrev:silver_hoe')
+	event.hide('indrev:lead_hoe')
+	event.hide('indrev:copper_hoe')
+	event.hide('indrev:bronze_hoe')
+
+	event.hide('indrev:tin_helmet')
+	event.hide('indrev:steel_helmet')
+	event.hide('indrev:silver_helmet')
+	event.hide('indrev:lead_helmet')
+	event.hide('indrev:copper_helmet')
+	event.hide('indrev:bronze_helmet')
+
+	event.hide('indrev:tin_chestplate')
+	event.hide('indrev:steel_chestplate')
+	event.hide('indrev:silver_chestplate')
+	event.hide('indrev:lead_chestplate')
+	event.hide('indrev:copper_chestplate')
+	event.hide('indrev:bronze_chestplate')
+
+	event.hide('indrev:tin_leggings')
+	event.hide('indrev:steel_leggings')
+	event.hide('indrev:silver_leggings')
+	event.hide('indrev:lead_leggings')
+	event.hide('indrev:copper_leggings')
+	event.hide('indrev:bronze_leggings')
+
+	event.hide('indrev:tin_boots')
+	event.hide('indrev:steel_boots')
+	event.hide('indrev:silver_boots')
+	event.hide('indrev:lead_boots')
+	event.hide('indrev:copper_boots')
+	event.hide('indrev:bronze_boots')
+
+	event.hide('appliedenergistics2:certus_quartz_sword')
+	event.hide('appliedenergistics2:nether_quartz_sword')
+
+	event.hide('appliedenergistics2:certus_quartz_shovel')
+	event.hide('appliedenergistics2:nether_quartz_shovel')
+
+	event.hide('appliedenergistics2:certus_quartz_pickaxe')
+	event.hide('appliedenergistics2:nether_quartz_pickaxe')
+
+	event.hide('appliedenergistics2:certus_quartz_axe')
+	event.hide('appliedenergistics2:nether_quartz_axe')
+
+	event.hide('appliedenergistics2:certus_quartz_hoe')
+	event.hide('appliedenergistics2:nether_quartz_hoe')
+
+	event.hide('appliedenergistics2:certus_quartz_helmet')
+	event.hide('appliedenergistics2:nether_quartz_helmet')
+
+	event.hide('appliedenergistics2:certus_quartz_chestplate')
+	event.hide('appliedenergistics2:nether_quartz_chestplate')
+
+	event.hide('appliedenergistics2:certus_quartz_leggings')
+	event.hide('appliedenergistics2:nether_quartz_leggings')
+
+	event.hide('appliedenergistics2:certus_quartz_boots')
+	event.hide('appliedenergistics2:nether_quartz_boots')
+})
